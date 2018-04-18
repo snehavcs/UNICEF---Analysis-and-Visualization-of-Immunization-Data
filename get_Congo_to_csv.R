bf0f3f80f2bd68b34639de411f9c693edf549ec9
Sneha Vasanth <snehavcs@gmail.com>
2018-04-17 22:23:17 -0700

    Predictive modeling files
    Feature identification for having immunization card

--- get_Congo_to_csv.R
+++
@@ -1,7 +0,0 @@
-setwd("C:/Users/sneha/Documents/GitHub/Unicef/Congo data")
-library(haven)
-library(data.table)
-ch.data <- read_spss("ch.sav")
-View(ch.data)
-
-write.csv(ch.data, file = "ch.csv",row.names=FALSE, na="")
