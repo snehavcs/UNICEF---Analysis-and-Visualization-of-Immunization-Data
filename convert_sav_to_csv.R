dbc890f7caa9983cec342010e4b6b1cfaaaec3d4
VaisakhiMishra <vm11@uw.edu>
2018-04-28 13:32:52 -0700

    Code Update

--- convert_sav_to_csv.R
+++ convert_sav_to_csv.R
@@ -35,3 +35,22 @@ dataset.labels3 <- makelist(hh.data)
 merged1 <- rbind(dataset.labels1,dataset.labels2)
 merged_final <- rbind(merged1, dataset.labels3)
 write.csv(merged_final, file = "label_pak.csv",row.names=FALSE, na="")
+
+setwd("C:/Users/Varsha/Desktop/MSIM/Capstone/Nigeria 2011 MICS_Datasets/Nigeria_MICS4_Datasets/Nigeria MICS 2011 SPSS Datasets")
+#Read the SAV files into R
+ch.data <- read_spss("ch.sav")
+View(ch.data)
+hh.data <- read_spss("hh.sav")
+View(hh.data)
+wm.data <- read_spss("hh.sav")
+View(wm.data)
+
+#Make Label List for all the files
+dataset.labels1 <- makelist(ch.data)
+dataset.labels2 <- makelist(wm.data)
+dataset.labels3 <- makelist(hh.data)
+
+#Merge all the files and print the combined label list as a csv file
+merged1 <- rbind(dataset.labels1,dataset.labels2)
+merged_final <- rbind(merged1, dataset.labels3)
+write.csv(merged_final, file = "label_nig.csv",row.names=FALSE, na="")
